8cf96c6a472c8bbc10dfc0d552a1f7ac017ed146
ThiyaraBandara <138982056+ThiyaraBandara@users.noreply.github.com>
2023-07-31 16:45:09 +0530

    Add files via upload

---
+++ C# Tutorials/Lab .cs(question 1-Program).cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = getSum(num1, num2);
+            Console.WriteLine("Sum: " + sum);
+        }
+
+        static int getSum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ C# Tutorials/Lab 3.cs(question 2 - Program).cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _230523___Lab_Session
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 2, 4, 6};
+            int[] y = { 3, 6, 9 };
+
+            int scoreX = 0;
+            int scoreY = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (x[i] > y[i])
+                {
+                    scoreX++;
+                } else if (x[i] < y[i])
+                {
+                    scoreY++;
+                }
+            }
+
+            Console.WriteLine("Anura's Score: " + scoreX);
+            Console.WriteLine("Bhagya's Score: " + scoreY);
+
+            
+        }
+    }
+}
